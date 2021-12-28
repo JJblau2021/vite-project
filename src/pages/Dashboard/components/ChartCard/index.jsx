cb4174b72be3bfe9f9976b39926c868adf0ac42b
Sun Haoliang <jjblau2021@163.com>
2021-12-28 21:39:24 +0800

    update commonChart

--- src/pages/Dashboard/components/ChartCard/index.jsx
+++ src/pages/Dashboard/components/ChartCard/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Card } from '@douyinfe/semi-ui';
+import CommonChart from '@/components/CommonChart';
 import styles from './index.module.less';
 
 const ChartCard = () => {
   return (
     <Card title="ChartCard" bordered={!1}>
-      blabla...
+      <CommonChart width="100%" height={400} resizeTrigger />
     </Card>
   );
 };
